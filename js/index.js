1f77daed43213824ebb8950bcca2f6c0cec208ed
vasuldutschak <vasuldutschak@mail.ru>
2023-01-11 15:51:34 +0200

    finished profiled card section

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
-
 function initBackgroundAnimation(bubbleContainer, bubbleBox) {
     const bubbleRotate = [{transform: 'rotate(0deg)'}, {transform: 'rotate(360deg)'}]
     const bubbleMoveDown = [{transform: 'translateY(-100%) scale(0)'}, {transform: `translateY(${innerHeight}px) scale(1)`}];
@@ -18,5 +17,23 @@ function initBackgroundAnimation(bubbleContainer, bubbleBox) {
                 }, randomIntFromInterval(300, 5000))
             })
 }
+function startTypingEffect(el,text){
+    el.innerText=text
+    el.style.width=`${text.length}ch`
+    el.animate([{borderColor:'#78cc6d'},{borderColor:'transparent'},{borderColor:'#78cc6d'}],
+        {
+            duration:750,
+            iterations: Infinity,
+            easing: 'cubic-bezier(0, 0, 1, 1)'
+        })
+    el.animate([{width:'0ch'},{width:`${text.length}ch`}],
+        {
+            duration:4000,
+            iterations: Infinity,
+            direction: 'alternate',
+            easing:`steps(${text.length})`,
+        })
+}
 
+startTypingEffect(document.querySelector('.subtitle-typed'), "I am a developer")
 initBackgroundAnimation('.bubble__container', '.bubble')
